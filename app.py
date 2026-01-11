74735451773fab00945f08a6a7f2c58ec8dcdc77
Ayushi-0311 <windwatcher17@gmail.com>
2026-01-11 20:15:16 +0530

    Career conversation chatbot

--- app.py
+++ app.py
@@ -1,44 +1,130 @@
-# read env key for openai
 from dotenv import load_dotenv
-# import OpenAI client
 from openai import OpenAI
-# to read pdf file and extract text
+import json
+import os
+import requests
 from pypdf import PdfReader
-# to make the UI
 import gradio as gr
-from pathlib import Path
+
 
 load_dotenv(override=True)
-# Create an instance of the OpenAI class
-openai = OpenAI()
 
-BASE_DIR = Path(__file__).parent
+def push(text):
+    requests.post(
+        "https://api.pushover.net/1/messages.json",
+        data={
+            "token": os.getenv("PUSHOVER_TOKEN"),
+            "user": os.getenv("PUSHOVER_USER"),
+            "message": text,
+        }
+    )
 
-reader = PdfReader(BASE_DIR / "me" / "Profile.pdf")
 
-linkedin = ""
-for page in reader.pages:
-    text = page.extract_text()
-    if text:
-        linkedin += text
+def record_user_details(email, name="Name not provided", notes="not provided"):
+    push(f"Recording {name} with email {email} and notes {notes}")
+    return {"recorded": "ok"}
 
-# Read the summary text
-with open(BASE_DIR / "me" / "summary.txt", encoding="utf-8") as f:
-    summary = f.read()
+def record_unknown_question(question):
+    push(f"Recording {question}")
+    return {"recorded": "ok"}
 
-name = "Ayushi Saxena"
-system_prompt = f"You are acting as {name}. You are answering questions on {name}'s website, \
-particularly questions related to {name}'s career, background, skills and experience. \
-Your responsibility is to represent {name} for interactions on the website as faithfully as possible. \
-You are given a summary of {name}'s background and LinkedIn profile which you can use to answer questions. \
+record_user_details_json = {
+    "name": "record_user_details",
+    "description": "Use this tool to record that a user is interested in being in touch and provided an email address",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "email": {
+                "type": "string",
+                "description": "The email address of this user"
+            },
+            "name": {
+                "type": "string",
+                "description": "The user's name, if they provided it"
+            }
+            ,
+            "notes": {
+                "type": "string",
+                "description": "Any additional information about the conversation that's worth recording to give context"
+            }
+        },
+        "required": ["email"],
+        "additionalProperties": False
+    }
+}
+
+record_unknown_question_json = {
+    "name": "record_unknown_question",
+    "description": "Always use this tool to record any question that couldn't be answered as you didn't know the answer",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "question": {
+                "type": "string",
+                "description": "The question that couldn't be answered"
+            },
+        },
+        "required": ["question"],
+        "additionalProperties": False
+    }
+}
+
+tools = [{"type": "function", "function": record_user_details_json},
+        {"type": "function", "function": record_unknown_question_json}]
+
+
+class Me:
+
+    def __init__(self):
+        self.openai = OpenAI()
+        self.name = "Ayushi Saxena"
+        self.linkedin = ""
+        with open("me/Resume.txt", "r", encoding="utf-8") as l:
+            self.linkedin = l.read()
+        with open("me/summary.txt", "r", encoding="utf-8") as f:
+            self.summary = f.read()
+
+
+    def handle_tool_call(self, tool_calls):
+        results = []
+        for tool_call in tool_calls:
+            tool_name = tool_call.function.name
+            arguments = json.loads(tool_call.function.arguments)
+            print(f"Tool called: {tool_name}", flush=True)
+            tool = globals().get(tool_name)
+            result = tool(**arguments) if tool else {}
+            results.append({"role": "tool","content": json.dumps(result),"tool_call_id": tool_call.id})
+        return results
+    
+    def system_prompt(self):
+        system_prompt = f"You are acting as {self.name}. You are answering questions on {self.name}'s website, \
+particularly questions related to {self.name}'s career, background, skills and experience. \
+Your responsibility is to represent {self.name} for interactions on the website as faithfully as possible. \
+You are given a summary of {self.name}'s background and LinkedIn profile which you can use to answer questions. \
 Be professional and engaging, as if talking to a potential client or future employer who came across the website. \
-If you don't know the answer, say so."
-system_prompt += f"\n\n## Summary:\n{summary}\n\n## LinkedIn Profile:\n{linkedin}\n\n"
-system_prompt += f"With this context, please chat with the user, always staying in character as {name}."
+If you don't know the answer to any question, use your record_unknown_question tool to record the question that you couldn't answer, even if it's about something trivial or unrelated to career. \
+If the user is engaging in discussion, try to steer them towards getting in touch via email; ask for their name, email and any message for me and record it using your record_user_details tool. "
 
-def chat(message, history):
-    messages = [{'role': 'system', 'content': system_prompt}] + history + [{'role': 'user', 'content': message}]
-    response = openai.chat.completions.create(model = "gpt-4o-mini", messages = messages)
-    return response.choices[0].message.content
+        system_prompt += f"\n\n## Summary:\n{self.summary}\n\n## LinkedIn Profile:\n{self.linkedin}\n\n"
+        system_prompt += f"With this context, please chat with the user, always staying in character as {self.name}."
+        return system_prompt
+    
+    def chat(self, message, history):
+        messages = [{"role": "system", "content": self.system_prompt()}] + history + [{"role": "user", "content": message}]
+        done = False
+        while not done:
+            response = self.openai.chat.completions.create(model="gpt-4o-mini", messages=messages, tools=tools)
+            if response.choices[0].finish_reason=="tool_calls":
+                message = response.choices[0].message
+                tool_calls = message.tool_calls
+                results = self.handle_tool_call(tool_calls)
+                messages.append(message)
+                messages.extend(results)
+            else:
+                done = True
+        return response.choices[0].message.content
+    
 
-gr.ChatInterface(chat).launch()
+if __name__ == "__main__":
+    me = Me()
+    gr.ChatInterface(me.chat).launch()
